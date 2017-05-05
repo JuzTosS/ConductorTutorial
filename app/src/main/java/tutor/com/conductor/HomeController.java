c95be726ab5ca7448151c7df0ce397943b1c08b2
Kirill Akhmetov <kahmetov@distillery.com>
2017-05-05 17:18:31 +0300

    Part 2.1

--- app/src/main/java/tutor/com/conductor/HomeController.java
+++ app/src/main/java/tutor/com/conductor/HomeController.java
@@ -8,8 +8,26 @@ import android.view.ViewGroup;
 import com.bluelinelabs.conductor.Controller;
 
 public class HomeController extends Controller {
+    private View tree;
+
+    @NonNull
     @Override
     protected View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container) {
-        return inflater.inflate(R.layout.controller_home, container, false);
+        View view = inflater.inflate(R.layout.controller_home, container, false);
+        tree = view.findViewById(R.id.tree);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tree.setVisibility(View.VISIBLE);
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    protected void onDestroyView(@NonNull View view) {
+        super.onDestroyView(view);
+        tree = null;
     }
 }
